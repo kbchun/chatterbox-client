74985b32744364934236d72503c2b7d2186e417a
kbchun <kbchun5712@gmail.com>
2016-09-27 16:45:08 -0700

    Refactor code and add styles

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -10,10 +10,39 @@ $(document).ready(function () {
     users: {},
 
     init: function () {
+      // submit message, send to server
+      $('#submit-text').on('click', function (event) {
+        var text = $('#text-box').val();
+        var username = window.location.search.slice(window.location.search.indexOf('=') + 1);
+        app.send({
+          'text': text,
+          'username': username,
+          'roomname': app.selectedRoom
+        });
+        $('#text-box').val('');
+      });
+
+      // change selected room
+      $('#roomButton').on('click', function (event) {
+        app.clearMessages();
+        app.selectedRoom = $('#room-selector option:selected').val();
+        app.firstFetch = true;
+        app.allMsgs = {};
+        app.fetch();
+      });
+
+      // add a new room 
+      $('#add-room').on('click', function (event) {
+        var newRoom = $('#room-input').val();
+        app.renderRoom(newRoom);
+      });
+
+      //initialize app
       app.fetch();
+
+      // poll for new messages
       setInterval(function () {
         app.fetch();
-        // app.send({username: 'kyle', text: 'spam'});
       }, 2000);
     },
 
@@ -40,7 +69,7 @@ $(document).ready(function () {
         url: app.server,
         type: 'GET',
         data: {
-          limit: 100,
+          limit: 1000000,
           order: '-createdAt'
         },
         contentType: 'application/json',
@@ -55,6 +84,7 @@ $(document).ready(function () {
       });
     },
 
+    // filter old messages and call render on new ones
     fetchCompleted: function (messages) {
       messages.forEach(function (msg) {
         if (!app.allMsgs.hasOwnProperty(msg.createdAt)) {
@@ -62,43 +92,48 @@ $(document).ready(function () {
           app.renderMessage(msg);
         }
       });
+      // tells render to prepend
       app.firstFetch = false;
-
     },
 
+    // remove all messages
     clearMessages: function () {
       $('#chats').empty();
     },
 
+    // put message on the screen
     renderMessage: function (message) {
-      var $messageBox = $('<div class="messageBox"></div>');
-      var $username = '<div class="username ' + filterXSS(message.username) + '"onClick=app.handleUsernameClick.call(this)>' + filterXSS(message.username) + '</div>';
+      // set username to anon if no name is given
+      if (!message.username) {
+        var username = 'Anonymous';
+      } else {
+        var username = message.username;
+      }
+
+      // message component
+      var $messageBox = $('<div class="messageBox ' + filterXSS(username) + '"></div>');
       var $message = '<div class="message">' + filterXSS(message.text) + '</div>';
-      var $timeStamp = '<div class="timeStamp">' + message.createdAt + '</div>';
+      var $timeStamp = '<div class="timeStamp">' + jQuery.timeago(message.createdAt) + '</div>';
+      var $username = '<div class="username" onClick=app.handleUsernameClick.call(this)>' 
+        + filterXSS(username) 
+        + '</div>';
       
       // automatically set undefined rooms and empty string rooms to lobby
       if (message.roomname === '' || message.roomname === undefined) {
-        var $room = '<div class="chatRoom">' + 'lobby' + '</div><br>';
         var roomname = 'lobby';
       } else {
         var roomname = filterXSS(message.roomname);
-        var $room = '<div class="chatRoom">' + filterXSS(roomname) + '</div><br>';
       }
 
       // creating message box
       $messageBox.append($username);
       $messageBox.append($message);
       $messageBox.append($timeStamp);
-      $messageBox.append($room);
 
       // creating list of unique rooms
-      if (!app.rooms.hasOwnProperty(roomname)) {
-        var option = $('<option value=' + roomname + '>' + roomname + '</option>');
-        $('#room-selector').append(option);
-        app.rooms[roomname] = roomname;
-      }
+      app.renderRoom(roomname);
 
-      // upon refreshing page, fetching only the first 100 messages
+      // upon refreshing page, fetching messages for selected room
       if (app.firstFetch && app.selectedRoom === roomname) {
         $('#chats').append($messageBox);
 
@@ -107,54 +142,34 @@ $(document).ready(function () {
         $('#chats').prepend($messageBox);      
       }
 
-      // checks if user is a friend; if so, bold their messages (!!!! currently just name is bolded)
+      // checks if user is a friend; if so, bold their messages
       if (app.users.hasOwnProperty(message.username)) {
-        $('.' + message.username).addClass('bold');
+        $('.' + message.username).addClass('befriended');
       }
     },
 
-    renderRoom: function () {
-
+    // add room to room picker list
+    renderRoom: function (roomname) {
+      roomname = roomname.toLowerCase();
+      if (!app.rooms.hasOwnProperty(roomname)) {
+        var option = $('<option value=' + roomname + '>' + roomname + '</option>');
+        $('#room-selector').append(option);
+        app.rooms[roomname] = roomname;
+        $('#room-input').val('');
+      }
     },
 
+    // add user to friends
     handleUsernameClick: function() {
       var name = this.textContent;
-      $('.' + name).addClass('bold');
+      $('.' + name).addClass('befriended');
       if (!app.users.hasOwnProperty(name)) {
         app.users[name] = name;
       } 
     }
   };
-
-  $('#submit-text').on('click', function (event) {
-    var text = $('#text-box').val();
-    var username = window.location.search.slice(window.location.search.indexOf('=') + 1);
-    app.send({
-      'text': text,
-      'username': username,
-      'roomname': app.selectedRoom
-    });
-    $('#text-box').val('');
-  });
-
-  $('#roomButton').on('click', function (event) {
-    app.clearMessages();
-    app.selectedRoom = $('#room-selector option:selected').val();
-    app.firstFetch = true;
-    app.allMsgs = {};
-    app.fetch();
-  });
-
-  $('#add-room').on('click', function (event) {
-    var newRoom = $('#room-input').val();
-    if (!app.rooms.hasOwnProperty(newRoom)) {
-      var option = $('<option value=' + newRoom + '>' + newRoom + '</option>');
-      $('#room-selector').append(option);
-      app.rooms[newRoom] = newRoom;
-      $('#room-input').val('');
-    }
-  });
-
+ 
+  // initialize app
   app.init();
 });
 
